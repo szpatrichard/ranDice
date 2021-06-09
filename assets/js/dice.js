d57da2b69fed7ba1fef2138b5222e8d7b2518377
szpatrikrichard <richpszil@gmail.com>
2021-06-09 10:26:22 +0100

    Refactor JS

--- assets/js/dice.js
+++ assets/js/dice.js
@@ -1,21 +1,32 @@
+// Create a class for the Dice
 class Dice {
+	// Array for holding the randomised results
 	results = [];
 
-	constructor(sides, amount) {
+	// Constructor of the class with the amount of die and sides as arguments
+	constructor(amount, sides) {
+		// Check the validity of the inputs (must be greater than 0)
 		if (sides > 0 && amount > 0) {
-			this._sides = sides;
+			// Assign the values to the fields of the class
 			this._amount = amount;
+			this._sides = sides;
 		} else {
+			// Throw an error for not having valid arguments supplied
 			throw Error("Invalid parameters.");
 		}
 	}
 
+	// Method for rolling the die
 	roll() {
+		// Produce the same amount of results as many die have been rolled
 		for (let i = 0; i < this._amount; i++) {
+			// Randomise the result of the roll
 			this.results.push(Math.floor(Math.random() * this._sides) + 1);
 		}
+
 		return this.results;
 	}
 }
 
+// Export the class
 export default Dice;
